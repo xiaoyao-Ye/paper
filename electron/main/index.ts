069a756067c79b069aa6d78b2fe8a0c0ca183dac
_Ghosteye <yqcyecong@163.com>
2024-12-04 09:59:01 +0800

    perf: using WebContentView

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain, screen, Menu } from 'electron'
+import { app, BrowserWindow, ipcMain, screen, WebContentsView } from 'electron'
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 import os from 'node:os'
@@ -37,12 +37,9 @@ if (!app.requestSingleInstanceLock()) {
   process.exit(0)
 }
 
-// Menu.setApplicationMenu(null)
-
 let win: BrowserWindow | null = null
 const preload = path.join(__dirname, '../preload/index.mjs')
 const indexHtml = path.join(RENDERER_DIST, 'index.html')
-// const indexHtml = path.join(RENDERER_DIST, 'background.html')
 
 async function createWindow() {
   win = new BrowserWindow({
@@ -50,13 +47,6 @@ async function createWindow() {
     icon: path.join(process.env.VITE_PUBLIC, 'logo_1024.png'),
     webPreferences: {
       preload,
-      // webSecurity: false,
-      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
-      // nodeIntegration: true,
-
-      // Consider using contextBridge.exposeInMainWorld
-      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
-      // contextIsolation: false,
     },
   })
 
@@ -68,18 +58,6 @@ async function createWindow() {
   } else {
     win.loadFile(indexHtml)
   }
-
-  // Test actively push message to the Electron-Renderer
-  // win.webContents.on('did-finish-load', () => {
-  //   win?.webContents.send('main-process-message', new Date().toLocaleString())
-  // })
-
-  // Make all links open with the browser, not with the application
-  // win.webContents.setWindowOpenHandler(({ url }) => {
-  //   if (url.startsWith('https:')) shell.openExternal(url)
-  //   return { action: 'deny' }
-  // })
-  // win.webContents.on('will-navigate', (event, url) => { }) #344
 
   win.on('closed', () => {
     app.dock.hide()
@@ -96,9 +74,7 @@ app.whenReady().then(async () => {
 
   const isAutoLaunch = app.getLoginItemSettings().wasOpenedAtLogin
   if (isAutoLaunch) {
-    console.log('应用是通过开机自启动打开的')
     app.dock.hide()
-    // win?.hide()
   } else {
     createWindow()
     // 手动显示, 抵消 setVisibleOnAllWorkspaces 的副作用, 首次打开时 opt + cmd + h 还是无法 hide win窗口
@@ -123,8 +99,7 @@ app.on('activate', () => {
   app.dock.show()
   const allWindows = BrowserWindow.getAllWindows()
   if (allWindows.length) {
-    // 找到非 wallpaperWindow 的窗口
-    const mainWindow = allWindows.find(w => w !== wallpaperWindow && w !== componentWindow)
+    const mainWindow = allWindows.find(w => w !== wallpaperWindow)
     if (mainWindow) {
       mainWindow.focus()
     } else {
@@ -181,16 +156,8 @@ function createWallpaperWindow() {
     // useContentSize: true,
     webPreferences: {
       preload,
-      // webSecurity: false,
-      // nodeIntegration: true,
-      // contextIsolation: false,
     },
   })
-
-  // wallpaperWindow.once('ready-to-show', () => {
-  //   console.log('ready-to-show')
-  //   wallpaperWindow.show()
-  // })
 
   wallpaperWindow.on('closed', () => {
     wallpaperWindow = null
@@ -202,15 +169,6 @@ function createWallpaperWindow() {
       visibleOnFullScreen: true,
     })
   }
-  // wallpaperWindow.on('hide', e => {
-  // 这种方式会有闪烁
-  //   wallpaperWindow.showInactive()
-  //   // wallpaperWindow.show()
-  // })
-
-  // 使覆盖全屏幕，包含 MacOS Menu (!!!没有退出,退出极其困难, 智能option + command + esc +回车)
-  // 这些属性能能覆盖 mac 菜单栏: pop-up-menu/status/screen-saver
-  // wallpaperWindow.setAlwaysOnTop(true, 'screen-saver')
 
   screen.on('display-metrics-changed', () => {
     const { width, height, x, y } = screen.getPrimaryDisplay().bounds
@@ -241,11 +199,6 @@ async function setWallpaper(_, value: CategoryValue | string) {
     wallpaperWindow.focus()
   }
 
-  // 防止其他窗口层级被覆盖
-  BrowserWindow.getAllWindows()
-    .filter(w => w !== wallpaperWindow)
-    .forEach(w => w.focus())
-
   wallpaperCategory = value
   store.set(WALLPAPER_CATEGORY_KEY, value)
 }
@@ -262,132 +215,57 @@ async function setCategoryWallpaper(value: CategoryValue) {
 ipcMain.on('set-wallpaper', setWallpaper)
 
 const COMPONENT_OPTION_KEY = 'component_option'
-let componentWindow: BrowserWindow | null = null
+let componentViews: Record<string, WebContentsView> = {}
 let componentOption: Option = { value: '', query: {} }
 interface Option {
   value: CategoryValue | string
   query: Record<string, string>
 }
-function createComponentWindow() {
-  // 获取主屏幕
-  const primaryDisplay = screen.getPrimaryDisplay()
-  const { width, height, x, y } = primaryDisplay.bounds
 
-  const w = width / 3
-  const h = height / 3
-
-  componentWindow = new BrowserWindow({
-    width: w,
-    height: h,
-    x: width - w,
-    y,
-    type: 'desktop', // 设置为桌面窗口
-    frame: false, // 无边框
-    transparent: true,
-    titleBarStyle: 'hidden',
-    roundedCorners: false, // 禁用圆角
-    // Windows 下必须配置，而 MacOS 则不需要（否则会打开一个全屏新桌面）
-    fullscreen: process.platform !== 'darwin',
-    // 使覆盖全屏幕，包含 MacOS Menu Bar
-    enableLargerThanScreen: true,
-    // Windows 下必须配置，否则会禁用视频播放
-    // type: 'toolbar',
-    // trafficLightPosition: { x: 999999, y: 999999 }, // 将红绿灯按钮移到看不见的位置
-    // show: false,
-    // resizable: false,
-    // movable: false,
-    // focusable: false,
-    // useContentSize: true,
-    webPreferences: {
-      preload,
-      // webSecurity: false,
-      // nodeIntegration: true,
-      // contextIsolation: false,
-    },
-  })
-
-  // wallpaperWindow.once('ready-to-show', () => {
-  //   console.log('ready-to-show')
-  //   wallpaperWindow.show()
-  // })
-
-  componentWindow.on('closed', () => {
-    componentWindow = null
-  })
-
-  if (process.platform === 'darwin') {
-    componentWindow.setVisibleOnAllWorkspaces(true, {
-      // 这种方式没闪烁, 但是 win 窗口无法触发 option + command + h 的 hide 效果,并且触发了类似 app.dock.hide() 的效果
-      visibleOnFullScreen: true,
-    })
-  }
-  // wallpaperWindow.on('hide', e => {
-  // 这种方式会有闪烁
-  //   wallpaperWindow.showInactive()
-  //   // wallpaperWindow.show()
-  // })
-
-  // 使覆盖全屏幕，包含 MacOS Menu (!!!没有退出,退出极其困难, 智能option + command + esc +回车)
-  // 这些属性能能覆盖 mac 菜单栏: pop-up-menu/status/screen-saver
-  // wallpaperWindow.setAlwaysOnTop(true, 'screen-saver')
-
-  // screen.on('display-metrics-changed', () => {
-  //   const { width, height, x, y } = screen.getPrimaryDisplay().bounds
-  //   const { width: w, height: h } = componentWindow.getBounds()
-  //   if (width === w && height === h) return
-  //   componentWindow.setSize(width, height, true)
-  //   componentWindow.setPosition(x, y, true)
-  // })
-}
 async function setComponent(_, value: CategoryValue | string, query: Record<string, string>) {
   if (componentOption.value === value && qs.stringify(componentOption.query) === qs.stringify(query)) return
 
-  if (!componentWindow) createComponentWindow()
+  if (!wallpaperWindow) createWallpaperWindow()
 
-  if (isCategoryValue(value, category)) {
-    await setCategoryComponent(value, query)
-  } else {
-    await componentWindow.loadURL(value)
+  const { width, height } = screen.getPrimaryDisplay().bounds
+  const w = width / 3
+  const h = height / 3
+
+  let view = componentViews[value]
+  // 已存在的组件更新 options, 不存在的组件才创建组件
+  if (!view) {
+    view = new WebContentsView()
+    view.setBackgroundColor('rgba(255,255,255,0)')
+    view.setBounds({ x: width - w, y: 0, width: w, height: h })
+    wallpaperWindow.contentView.addChildView(view)
+    componentViews[value] = view
   }
 
-  if (!componentOption.value) {
+  if (isCategoryValue(value, category)) {
+    const hash = `${value}?${qs.stringify(query)}`
+    if (VITE_DEV_SERVER_URL) {
+      await view.webContents.loadURL(`${VITE_DEV_SERVER_URL}#/${hash}`)
+      view.webContents.openDevTools()
+    } else {
+      await view.webContents.loadFile(indexHtml, { hash })
+    }
+    // 更新 options 需要重新加载
+    view.webContents.reload()
+  } else {
+    await view.webContents.loadFile(value)
+  }
+
+  // 抵消 client 无法关闭的副作用
+  if (!wallpaperCategory) {
     app.dock.show()
-    componentWindow.focus()
+    wallpaperWindow.focus()
   }
   componentOption.value = value
   componentOption.query = query
   store.set(COMPONENT_OPTION_KEY, componentOption)
 }
-async function setCategoryComponent(value: CategoryValue, query: Record<string, string>) {
-  const hash = `${value}?${qs.stringify(query)}`
-  if (VITE_DEV_SERVER_URL) {
-    await componentWindow.loadURL(`${VITE_DEV_SERVER_URL}#/${hash}`)
-    componentWindow.webContents.reload()
-    componentWindow.webContents.openDevTools()
-  } else {
-    await componentWindow.loadFile(indexHtml, { hash })
-    componentWindow.webContents.reload()
-    // await componentWindow.loadFile(indexHtml, { hash: value, query }) // electron 会将 hash 拼接到 query 后面
-  }
-}
+
 ipcMain.on('set-component', setComponent)
-
-// New window example arg: new windows url
-// ipcMain.handle('open-win', (_, arg) => {
-//   const childWindow = new BrowserWindow({
-//     webPreferences: {
-//       preload,
-//       nodeIntegration: true,
-//       contextIsolation: false,
-//     },
-//   })
-
-//   if (VITE_DEV_SERVER_URL) {
-//     childWindow.loadURL(`${VITE_DEV_SERVER_URL}#${arg}`)
-//   } else {
-//     childWindow.loadFile(indexHtml, { hash: arg })
-//   }
-// })
 
 ipcMain.on('event', (_, ...args) => {
   wallpaperWindow.webContents.send('event', ...args)
